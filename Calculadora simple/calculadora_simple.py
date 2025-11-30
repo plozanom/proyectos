1f5de6339d954c177ebc8d563a462fd080f81c5d
Pedro Lozano <zamekh@hotmail.com>
2025-11-29 19:17:52 -0500

    calculadora simple

---
+++ Calculadora simple/calculadora_simple.py
@@ -0,0 +1,21 @@
+print(
+    "Este 'programa' requiere que el usuario ingrese dos numeros y este le devolverá la suma, resta, multiplicación, división y división exacta de esos numeros"
+)
+
+num1 = float(input("Ingrese su primer numero: "))
+num2 = float(input("Ingrese su segundo numero: "))
+
+# Suma
+print(f"La suma de los dos numeros es {num1 + num2}")
+
+# Resta
+print(f"La resta de los dos numeros es {num1 - num2}")
+
+# Multiplicación
+print(f"La multiplicación de los dos numeros es {num1 * num2}")
+
+# División
+print(f"La división de los dos numeros es {num1 / num2}")
+
+# División exacta
+print(f"La división exacta de los dos numeros es {num1 // num2}")
