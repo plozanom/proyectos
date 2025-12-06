91a9796cb703d257a2cdf695ff3818f97c5c3f6b
Pedro Lozano <zamekh@hotmail.com>
2025-12-05 23:44:24 -0500

    Haciendo cambios en la calculadora de potencias simple 2

--- Calculadora de potencia/potencia.py
+++ Calculadora de potencia/potencia.py
@@ -1,5 +1,5 @@
 # Esta es una calculadora de potencias que solo funciona (de momento) con numeros enteros positivos en la parte del exponente
-# Además es probable que proboque errores si se utilizan el cero y valores negativos
+# Además es probable que provoque errores si se utilizan el cero y valores negativos
 # Hay un montón de cosas que mejorar
 
 def potencia(base = 1, exponente = 1):
