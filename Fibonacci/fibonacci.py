646e8fc6e65d04c1242665d4f68bf81dd4feaacc
Pedro Lozano <zamekh@hotmail.com>
2025-12-07 02:31:44 -0500

    Agregando una función que calcula el n-esimo termino de la secuencia de Fibonacci

---
+++ Fibonacci/fibonacci.py
@@ -0,0 +1,8 @@
+# Una función que calcula el n-esimo término de la secuencia de Fibonacci
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
