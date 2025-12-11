52fb9cd3f72b1d86f9ee04a39021952478c16387
Pedro Lozano <zamekh@hotmail.com>
2025-12-11 01:01:57 -0500

    Agregando buscador de numeros

---
+++ Buscador de numeros/buscador.py
@@ -0,0 +1,7 @@
+# Funcion que recibe una lista de numeros y un numero y retorna True si el numero se encuentra en la lista y False si no
+
+def buscador(lista, numero):
+
+    if numero in lista:
+        return True
+    return False
