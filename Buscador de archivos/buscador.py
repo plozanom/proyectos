8893cc71b6fe4b0315159b664b8f0fa6184be404
Pedro Lozano <zamekh@hotmail.com>
2025-12-19 00:12:36 -0500

    Haciendo que la función busque en un directorio

--- Buscador de archivos/buscador.py
+++ Buscador de archivos/buscador.py
@@ -1,7 +1,8 @@
 import os
 
-def buscar_archivos(ruta_inicial):#, nombre_archivo):
+def buscar_archivos(ruta_inicial, nombre_archivo):
 
     with os.scandir(ruta_inicial) as dirs:
         for dir in dirs:
-            print(f"Ruta: {dir.path}")
+            if dir.is_file() and nombre_archivo.lower() in dir.name.lower():
+                print(f"Ruta: {dir.path}")
