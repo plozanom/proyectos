e3d9c7f261c542feeb75d9c0c72e2221cf7deb07
Pedro Lozano <zamekh@hotmail.com>
2025-12-19 00:27:57 -0500

    Haciendo que la función sea recursiva y agregando excepciones

--- Buscador de archivos/buscador.py
+++ Buscador de archivos/buscador.py
@@ -2,7 +2,15 @@ import os
 
 def buscar_archivos(ruta_inicial, nombre_archivo):
 
-    with os.scandir(ruta_inicial) as dirs:
-        for dir in dirs:
-            if dir.is_file() and nombre_archivo.lower() in dir.name.lower():
-                print(f"Ruta: {dir.path}")
+    try:
+        with os.scandir(ruta_inicial) as dirs:
+            for dir in dirs:
+                if dir.is_file() and nombre_archivo.lower() in dir.name.lower():
+                    print(f"Ruta: {dir.path}")
+                elif dir.is_dir(follow_symlinks=False):
+                    buscar_archivos(dir.path, nombre_archivo)
+
+    except FileNotFoundError:
+        print(f"La ruta {ruta_inicial} no existe")
+    except PermissionError:
+        print(f"No tienes permiso para la ruta {ruta_inicial}")
