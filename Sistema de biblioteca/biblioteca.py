5fa68ffd635201ef4bb91898981c730d260208a3
Pedro Lozano <zamekh@hotmail.com>
2026-01-24 01:02:58 -0500

    Modificando la clase 'Libro' del Siatema de biblioteca

--- Sistema de biblioteca/biblioteca.py
+++ Sistema de biblioteca/biblioteca.py
@@ -1,8 +1,21 @@
 class Libro:
-    pass
+    def __init__(self, titulo, autor):
+        self.titulo = titulo
+        self.autor = autor
+        self.esta_prestado = False
+
+    def mostrar_info(self):
+        if self.esta_prestado:
+            estado = "Prestado"
+        else:
+            estado = "Disponible"
+
+        return f"Titulo: {self.titulo}\nAutor: {self.autor}\nEstado: {estado}"
+
 
 class Usuario:
     pass
+
 
 class Prestamo:
     pass
