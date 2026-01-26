17ae0f1cd305615df0023e828623a8eb2609eb86
Pedro Lozano <zamekh@hotmail.com>
2026-01-25 23:39:13 -0500

    Modificando las clases 'Usuario' y 'Prestamo' del Sistema de biblioteca

--- Sistema de biblioteca/biblioteca.py
+++ Sistema de biblioteca/biblioteca.py
@@ -14,8 +14,34 @@ class Libro:
 
 
 class Usuario:
-    pass
+    def __init__(self, nombre, edad):
+        self.tarjeta = False
+        self.nombre = nombre
+        self.edad = edad
+
+    def info_usuario(self):
+        if self.tarjeta:
+            membresia = "Tiene credenciales"
+        else:
+            membresia = "Debe diligenciar la membresía"
+
+        return f"Nombre: {self.nombre}\nEdad: {self.edad}\nMembresía: {membresia}"
 
 
-class Prestamo:
-    pass
+class Prestamo(Libro, Usuario):
+    def __init__(self, titulo, autor, nombre, edad, libros_prestados):
+        Libro.__init__(self, titulo, autor)
+        Usuario.__init__(self, nombre, edad)
+        self.libros_prestados = libros_prestados
+
+    def maximo_prestamo_libros(self):
+        if self.edad >= 18:
+            return 5
+        else:
+            return 2
+
+    def puede_prestar_libros(self):
+        if self.maximo_prestamo_libros() > self.libros_prestados:
+            return "Puede prestar libros"
+        else:
+            return "No puede prestar más libros"
