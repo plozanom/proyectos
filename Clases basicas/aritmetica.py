225a4860248de76a98637b23dc285810f5978723
Pedro Lozano <zamekh@hotmail.com>
2026-02-04 02:20:29 -0500

    Haciendo encapsulamiento a la clase Aritmetica

--- Clases basicas/aritmetica.py
+++ Clases basicas/aritmetica.py
@@ -1,7 +1,23 @@
 class Aritmetica:
     def __init__(self, operando1, operando2):
-        self.operando1 = operando1
-        self.operando2 = operando2
+        self.__operando1 = operando1
+        self.__operando2 = operando2
+
+    @property
+    def operando1(self):
+        return self.__operando1
+
+    @property
+    def operando2(self):
+        return self.__operando2
+
+    @operando1.setter
+    def operando1(self, operando1):
+        self.__operando1 = operando1
+
+    @operando2.setter
+    def operando2(self, operando2):
+        self.__operando2 = operando2
 
     def suma(self):
         return self.operando1 + self.operando2
