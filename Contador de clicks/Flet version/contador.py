82ef5590bbf0b4b5c5076abac4c1d0feb8da3500
Pedro Lozano <zamekh@hotmail.com>
2026-02-08 23:55:37 -0500

    Cambiando algunas cosas mientras entiendo el codigo

--- Contador de clicks/Flet version/contador.py
+++ Contador de clicks/Flet version/contador.py
@@ -2,24 +2,24 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.title = "Flet counter example"
+    page.title = "Contador de clicks"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
     input = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
 
-    def minus_click(e):
+    def click_negativo(e):
         input.value = str(int(input.value) - 1)
 
-    def plus_click(e):
+    def click_positivo(e):
         input.value = str(int(input.value) + 1)
 
     page.add(
         ft.Row(
             alignment=ft.MainAxisAlignment.CENTER,
             controls=[
-                ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
+                ft.IconButton(ft.Icons.REMOVE, on_click=click_negativo),
                 input,
-                ft.IconButton(ft.Icons.ADD, on_click=plus_click),
+                ft.IconButton(ft.Icons.ADD, on_click=click_positivo),
             ],
         )
     )
