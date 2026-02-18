a76b2ad69d961a27eb5a7a13199227c948af2e80
Pedro Lozano <zamekh@hotmail.com>
2026-02-17 23:54:24 -0500

    Borrado de un autor usando codigo mejor escrito al usar context manager

---
+++ Db - Biblioteca/borrado_con _context_manager.py
@@ -0,0 +1,22 @@
+import sqlite3
+from contextlib import closing
+
+database = "biblioteca.db"
+
+try:
+    with closing(sqlite3.connect(database)) as conexion:
+        # Importante: activar PRAGMA dentro de la conexión
+        conexion.execute("PRAGMA foreign_keys = ON")
+
+        with conexion:  # Inicia transacción
+            cursor = conexion.cursor()
+            # Intentamos borrar un autor que tiene libros
+            cursor.execute("DELETE FROM autores WHERE id = 1")
+
+# El error ocurre cuando se intenta borrar algo que daña la integridad de la base de datos, en este caso, un autor que tiene vinculados libros
+except sqlite3.IntegrityError as e:
+    print(
+        f"Error de integridad: No se puede borrar porque tiene libros asociados. Detalle: {e}"
+    )
+except sqlite3.Error as e:
+    print(f"Error general de SQLite: {e}")
