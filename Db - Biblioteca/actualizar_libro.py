524611625bbb416662b24bb2c6558945abc4d1fe
Pedro Lozano <zamekh@hotmail.com>
2026-02-17 23:48:53 -0500

    Utilizando context manager para mejorar el codigo de 'actualizar_libro.py'

--- Db - Biblioteca/actualizar_libro.py
+++ Db - Biblioteca/actualizar_libro.py
@@ -1,24 +1,30 @@
 import sqlite3
+from contextlib import closing
 
-conexion = sqlite3.connect("biblioteca.db")
-cursor = conexion.cursor()
+database = "biblioteca.db"
 
-# Se cambiará el titulo del libro con ID 4
-nuevo_titulo = "Don Quijote de la Mancha (Edición Revisada)"
-id_buscado = 4
+try:
+    with closing(sqlite3.connect(database)) as conexion:
+        with conexion:
+            cursor = conexion.cursor()
 
-# Actualización del nombre del libro
-# Nota: Siempre poner un WHERE en la consulta o sino todos los datos en la tabla se actualizarán
-cursor.execute(
-    """
-    UPDATE libros
-    SET titulo = ?
-    WHERE id = ?""",
-    (nuevo_titulo, id_buscado),
-)
+            # Se cambiará el titulo del libro con ID 4
+            nuevo_titulo = "Don Quijote de la Mancha (Edición Revisada)"
+            id_buscado = 4
 
-conexion.commit()
-print(
-    f"Filas actualizadas: {cursor.rowcount}"
-)  # rowcount cuenta cuantas filas han sido afectadas por la instrucción dada
-conexion.close()
+            # Actualización del nombre del libro
+            # Nota: Siempre poner un WHERE en la consulta o sino todos los datos en la tabla se actualizarán
+            cursor.execute(
+                """
+                UPDATE libros
+                SET titulo = ?
+                WHERE id = ?""",
+                (nuevo_titulo, id_buscado),
+            )
+
+            print(
+                f"Filas actualizadas: {cursor.rowcount}"
+            )  # rowcount cuenta cuantas filas han sido afectadas por la instrucción dada
+
+except sqlite3.Error as e:
+    print(f"Error general de SQLite: {e}")
